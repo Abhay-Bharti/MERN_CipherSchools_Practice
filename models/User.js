d64cb93d24a81dfc68ae9f6ba8aeca8f2c80948b
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2024-07-11 11:31:58 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { model, Schema } = require("mongoose");
+const { isEmail } = require("validator");
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: true,
+        validate: {
+            validator(email) {
+                return isEmail(email);
+            }
+        }
+    },
+    age: {
+        type: Number,
+        required: true,
+        validate: {
+            validator(age) {
+                if (age < 0) {
+                    throw new Error(`Age should be greater than 0`);
+                }
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+UserSchema.statics.findByEmailAndPasswordForAuth = async (email, password) => {
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error("Invalid Credentials");
+        }
+        if (password != user.password) {
+            throw new Error("Invalid Credentials");
+        }
+        console.log(`Login Successful`);
+        return user;
+    }
+    catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+const UserModel = model("User", UserSchema);
+
+module.exports = UserModel;
